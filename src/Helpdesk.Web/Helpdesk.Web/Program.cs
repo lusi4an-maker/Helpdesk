136cc1c4da665b09218c026c3de8d66177b4e3d2
lx1 <lusi4an@gmail.com>
2026-07-04 08:18:07 -0300

    connect api to web

--- src/Helpdesk.Web/Helpdesk.Web/Program.cs
+++ src/Helpdesk.Web/Helpdesk.Web/Program.cs
@@ -7,6 +7,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var uriString = builder.Configuration["ApiSettings:BaseUri"];
+
+if (Uri.TryCreate(uriString, UriKind.Absolute, out var uriSettings))
+{
+    builder.Services.AddScoped(sp => new HttpClient {BaseAddress = uriSettings });
+}
+else
+{
+    throw new Exception("La Uri base no esta bien configurada. Verifique.");
+}
 
 await builder.Build().RunAsync();
