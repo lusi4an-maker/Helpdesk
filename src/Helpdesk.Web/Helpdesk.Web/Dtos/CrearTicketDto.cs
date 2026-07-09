6a2bac0bb10cbb99187b81c16e98865bb5a29645
lx1 <lusi4an@gmail.com>
2026-07-09 13:09:24 -0300

    agrego dialog de ticket y cinta de opciones sobre la tabla

---
+++ src/Helpdesk.Web/Helpdesk.Web/Dtos/CrearTicketDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Helpdesk.Web.Dtos;
+
+public class CrearTicketDto
+{
+    public string Titulo { get; set; } = string.Empty;
+    public string Descripcion { get; set; } = string.Empty;
+}
--- src/Helpdesk.Web/Helpdesk.Web/Services/TicketService.cs
+++ src/Helpdesk.Web/Helpdesk.Web/Services/TicketService.cs
@@ -1,12 +1,13 @@
-﻿using Helpdesk.Web.Dtos;
+﻿using System.Net.Http.Json;
+using Helpdesk.Web.Dtos;
 using Helpdesk.Web.Json;
-using System.Net.Http.Json;
 
 namespace Helpdesk.Web.Services;
 
 public interface ITicketService
 {
     Task<TicketDto[]> GetTicketsAsync();
+    Task<TicketDto?> CreateTicketAsync(CrearTicketDto dto);
 }
 
 public class TicketService(HttpClient http) : ITicketService
@@ -14,5 +15,19 @@ public class TicketService(HttpClient http) : ITicketService
     public async Task<TicketDto[]> GetTicketsAsync()
     {
         return await http.GetFromJsonAsync<TicketDto[]>("tickets", JsonConfig.Options) ?? [];
+    }
+
+    public async Task<TicketDto?> CreateTicketAsync(CrearTicketDto dto)
+    {
+        HttpResponseMessage response = await http.PostAsJsonAsync("tickets",dto);
+        if (response.IsSuccessStatusCode) 
+        {
+            var succesful = await response.Content.ReadFromJsonAsync<TicketDto>(JsonConfig.Options);
+            return succesful;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
